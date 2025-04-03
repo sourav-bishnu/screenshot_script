43842896a9a1fec8356601203ba5e5b349b4e804
sourav-bishnu <souravbishnu2023@gmail.com>
2025-04-03 14:35:22 +0530

    changes

--- python_screenshot.py
+++ python_screenshot.py
@@ -1,3 +1,3 @@
 import pyautogui
 screenshot = pyautogui.screenshot()
-screenshot.save("screenshot.png")
+screenshot.save("screenshot.png")# this script will take a screenshot of the entire your screen and save it as "screenshot.png" you can change your screenshot file name.
